e6d53e734bce86d1ed241787b6c2316bb9531bbc
criatura435 <127697449+criatura435@users.noreply.github.com>
2023-04-15 13:46:02 -0500

    Starter code for user prompts

--- assets/script.js
+++ assets/script.js
@@ -6,10 +6,21 @@ var generateBtn = document.querySelector("#generate");
 // password character types (lower, upper, numeric, spec. char.) at least one
 // write password to textarea
 
-// writePassword function
-function writePassword(){
+// passwordParameters function for user prompts
+function passwordParameters(){
+  
+  // need this to check length, provide feedback, ask for input again; until correct
+  var passwordLength = window.prompt("How long would you like your password to be? Please choose a number from 8 to 128");
+  console.log(passwordLength);
+  
 
+  
+  var passwordLower = window.confirm("Would you like to include lowercase characters in your password? Ok for Yes or Cancel for No");
+  console.log(passwordLower);
 }
+
+passwordParameters();
+
 
 // Write password to the #password input
 function writePassword() {
