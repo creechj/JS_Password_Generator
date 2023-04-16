929e3dc6417152e683f7d40e8ef63e23c567b557
criatura435 <127697449+criatura435@users.noreply.github.com>
2023-04-16 13:39:16 -0500

    Char. Length If logic working; Other prompts added

--- assets/script.js
+++ assets/script.js
@@ -6,20 +6,35 @@ var generateBtn = document.querySelector("#generate");
 // password character types (lower, upper, numeric, spec. char.) at least one
 // write password to textarea
 
-// passwordParameters function for user prompts
-function passwordParameters(){
-  
-  // need this to check length, provide feedback, ask for input again; until correct
-  var passwordLength = window.prompt("How long would you like your password to be? Please choose a number from 8 to 128");
-  console.log(passwordLength);
-  
-
-  
-  var passwordLower = window.confirm("Would you like to include lowercase characters in your password? Ok for Yes or Cancel for No");
-  console.log(passwordLower);
+// function for password length user prompt
+// uses if logic to check for correct length range and loops until correct
+var passwordCharacters
+function passwordLength() {
+    passwordCharacters = window.prompt("How long would you like your password to be? Please choose a number from 8 to 128");
+    console.log(passwordCharacters);
+    if (passwordCharacters < 8) {
+      window.alert("Password must be 8-128 characters long.");
+      passwordLength();
+    } else if (passwordCharacters > 128) {
+      window.alert("Password must be 8-128 characters long.");
+      passwordLength();
+    } else if (passwordCharacters == null){
+      window.alert("Password must be 8-128 characters long.");
+      passwordLength();
+    }
 }
 
-passwordParameters();
+passwordLength();
+
+// user confirms for character type inclusions
+var passwordLower = window.confirm("Would you like to include lowercase characters in your password? Ok for Yes or Cancel for No");
+console.log(passwordLower);
+
+var passwordUpper = window.confirm("Would you like to include uppercase characters in your password? Ok for Yes or Cancel for No");
+console.log(passwordUpper);
+
+var passwordSpecial = window.confirm("Would you like to include special characters in your password? Ok for Yes or Cancel for No");
+console.log(passwordSpecial);
 
 
 // Write password to the #password input
